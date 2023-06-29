7c735639bb7b6de9397843aa9925961061df1aae
Tri Nguyen <ntri1805@gmail.com>
2023-06-29 15:47:17 +0700

    Implement evidently

--- src/model_predictor.py
+++ src/model_predictor.py
@@ -12,10 +12,24 @@ from fastapi import FastAPI, Request
 from pandas.util import hash_pandas_object
 from pydantic import BaseModel
 
-from problem_config import ProblemConst, create_prob_config
+from problem_config import ProblemConst, create_prob_config, get_prob_config
 from raw_data_processor import RawDataProcessor
 from utils import AppConfig, AppPath
 
+from evidently.report import Report
+from evidently import ColumnMapping
+
+
+from evidently import dashboard
+from evidently.test_preset import DataStabilityTestPreset
+from evidently.test_preset import DataQualityTestPreset
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset
+from evidently.dashboard.tabs import DataDriftTab, CatTargetDriftTab
+from evidently import ColumnMapping
+from evidently.report import Report
+from evidently.metrics import DatasetDriftMetric
+
+from evidently.dashboard import Dashboard
 
 PREDICTOR_API_PORT = 8000
 
@@ -50,9 +64,55 @@ class ModelPredictor:
         )
         self.model = mlflow.pyfunc.load_model(model_uri)
 
-    def detect_drift(self, feature_df) -> int:
+    def detect_drift(self,feature_df) -> int:
         # watch drift between coming requests and training data
-        return random.choice([0, 1])
+        drift_report = Report(metrics=[DatasetDriftMetric()])
+        
+        # get reference_df from train_data
+        train_x, train_y = RawDataProcessor.load_train_data(self.prob_config)
+        #train_x = train_x.to_numpy()
+        #train_y = train_y.to_numpy()
+        reference_df = pd.concat([train_x, train_y], axis=1)
+        print(reference_df)
+        print(feature_df)
+        logging.info(feature_df.shape)
+        logging.info(reference_df.shape)
+        print(reference_df[:10])
+
+        column_mapping = ColumnMapping()
+        # problem_1
+        cols_cat = ["feature1", "feature2"]
+        cols_num = ["feature3", "feature4", "feature5", 
+        "feature6", "feature7", "feature8", "feature8", 
+        "feature9", "feature10", "feature11", "feature12", 
+        "feature13", "feature14", "feature15", "feature16"]
+
+        # problem_2
+        num_cols = ["feature2", "feature5", "feature13", "feature18"]
+        cat_cols = [
+    "feature1", "feature3", "feature4", "feature6", "feature7",
+    "feature8", "feature9", "feature10", "feature11", "feature12",
+    "feature14", "feature15", "feature16", "feature17", "feature19", "feature20"]
+
+        column_mapping.categorical_features = cat_cols
+        column_mapping.numerical_features = num_cols
+        drift_report.run(current_data=feature_df, reference_data=reference_df[:10], column_mapping=column_mapping)
+
+        
+        #logging.info("drift results", drift_results)
+        drift_results = drift_report.as_dict()
+        print("Drift results", drift_results)
+        drift_score = drift_results["metrics"][0]["result"]["dataset_drift"]
+        # Set a threshold and return 1 if drift_score exceeds the threshold, otherwise 0
+        #drift_threshold = 0.8
+        #if drift_score > drift_threshold:
+        #    return 1
+        #return 0
+
+        if drift_score == True:
+            return 1
+        return 0 
+
 
     def predict(self, data: Data):
         start_time = time.time()
@@ -70,6 +130,7 @@ class ModelPredictor:
         )
 
         prediction = self.model.predict(feature_df)
+        logging.info("prediction", prediction)
         is_drifted = self.detect_drift(feature_df)
 
         run_time = round((time.time() - start_time) * 1000, 0)
@@ -104,7 +165,11 @@ class PredictorApi:
         @self.app.post("/phase-1/prob-1/predict")
         async def predict(data: Data, request: Request):
             self._log_request(request)
+            print("data")
             response = self.predictor.predict(data)
+            #logging.warning("prob1)
+            logging.warning("Response for prob1", response)
+            print(response)
             self._log_response(response)
             return response
 
@@ -112,16 +177,17 @@ class PredictorApi:
         async def predict(data: Data, request: Request):
             self._log_request(request)
             response = self.predictor_2.predict(data)
+            logging.warning("prob2", response) 
+            print(response)
             self._log_response(response)
             return response
-
     @staticmethod
     def _log_request(request: Request):
-        pass
+        logging.info(request)
 
     @staticmethod
     def _log_response(response: dict):
-        pass
+        logging.info(response)
 
     def run(self, port):
         uvicorn.run(self.app, host="0.0.0.0", port=port)
@@ -147,10 +213,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config-path", type=str, default=default_config_path)
     parser.add_argument("--config-path-2", type=str, default = default_config_path_2)
+   # parser.add_argument("--phase-id", type=str, default=ProblemConst.PHASE1)
+   # parser.add_argument("--prob-id", type=str, default=ProblemConst.PROB1)
+   # parser.add_argument("--phase-id-2", type=str, default=ProblemConst.PHASE1)
+   # parser.add_argument("--prob-id-2", type=str, default=ProblemConst.PROB1)
+
     parser.add_argument("--port", type=int, default=PREDICTOR_API_PORT)
     args = parser.parse_args()
 
-    predictor = ModelPredictor(config_file_path=args.config_path)
-    predictor_2 = ModelPredictor(config_file_path=args.config_path_2)
+    #problem_config = get_prob_config(args.phase_id, args.prob_id)
+    #problem_config_2 = get_prob_config(args.phase_id_2, args.prob_id_2)
+
+    predictor = ModelPredictor(config_file_path=args.config_path) #problem_config=problem_config)
+    predictor_2 = ModelPredictor(config_file_path=args.config_path_2) # problem_config=problem_config_2)
+
     api = PredictorApi(predictor, predictor_2)
+    #api.pred
     api.run(port=args.port)
